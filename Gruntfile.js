1dfb5ccc55e725ff4b6a62796764ad2043fec6af
edwardkerry <edwardkerry@gmail.com>
2016-01-29 18:16:29 +0000

    Adds Grunt scaffold

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+
+   });
+
+   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+};
