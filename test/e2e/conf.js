55cdea252cfd398114f62b2ad89226a96091ef4f
edwardkerry <edwardkerry@gmail.com>
2016-02-01 15:08:17 +0000

    Adds firefox to e2e.conf.js

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['toDoListFeature.js', 'filterFeature.js'],
-  capabilities: {
-    browserName: 'chrome'
-  }
+  multiCapabilities: [
+    {browserName: 'chrome'},{browserName: 'firefox'}
+  ]
 };
